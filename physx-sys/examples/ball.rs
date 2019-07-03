7d5463629d77cf94fa9866593e572d38e07a4f5b
Maik Klein <maik.klein@embark-studios.com>
2019-07-03 15:29:26 +0200

    Cleanup the ball example

--- physx-sys/examples/ball.rs
+++ physx-sys/examples/ball.rs
@@ -5,20 +5,25 @@ fn main() {
     unsafe {
         let foundation = physx_create_foundation();
         let physics = physx_create_physics(foundation);
+
         let mut scene_desc = PxSceneDesc_new(PxPhysics_getTolerancesScale(physics));
         scene_desc.gravity = PxVec3 {
             x: 0.0,
             y: -9.81,
             z: 0.0,
         };
+
         let dispatcher = phys_PxDefaultCpuDispatcherCreate(1, null_mut());
         scene_desc.cpuDispatcher = dispatcher as *mut PxCpuDispatcher;
         scene_desc.filterShader = get_default_simulation_filter_shader();
+
         let scene = PxPhysics_createScene_mut(physics, &scene_desc);
+
         let material = PxPhysics_createMaterial_mut(physics, 0.5, 0.5, 0.6);
         let ground_plane =
             phys_PxCreatePlane(physics, &PxPlane_new_1(0.0, 1.0, 0.0, 0.0), material);
         PxScene_addActor_mut(scene, ground_plane as *mut PxActor, null_mut());
+
         let sphere_geo = PxSphereGeometry_new_1(10.0);
         let sphere = phys_PxCreateDynamic(
             physics,
@@ -34,6 +39,7 @@ fn main() {
         );
         PxRigidBody_setAngularDamping_mut(sphere as *mut PxRigidBody, 0.5);
         PxScene_addActor_mut(scene, sphere as *mut PxActor, null_mut());
+
         let heights_over_time = (0..100)
             .map(|_| {
                 PxScene_simulate_mut(scene, 0.1, null_mut(), null_mut(), 0, true);
@@ -45,17 +51,18 @@ fn main() {
             })
             .collect::<Vec<_>>();
         let max_h = 18;
-        let lines = (0..max_h).map(|h| {
-            let h = max_h - 1 - h;
-            heights_over_time
-                .iter()
-                .enumerate()
-                .map(|(_t, p)| if h == *p { 'o' } else { ' ' })
-                .collect::<String>()
-        });
-        for l in lines {
-            println!("{}", l);
-        }
+        (0..max_h)
+            .map(|h| {
+                let h = max_h - 1 - h;
+                heights_over_time
+                    .iter()
+                    .enumerate()
+                    .map(|(_t, p)| if h == *p { 'o' } else { ' ' })
+                    .collect::<String>()
+            })
+            .for_each(|line| {
+                println!("{}", line);
+            });
         PxScene_release_mut(scene);
         PxDefaultCpuDispatcher_release_mut(dispatcher);
         PxPhysics_release_mut(physics);
