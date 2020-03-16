c5f985f4bd1781f09abcd8e37b46f5e4afeb9a64
Jake Shadle <jake.shadle@embark-studios.com>
2020-03-16 09:39:32 +0100

    Format comments

--- physx-sys/src/lib.rs
+++ physx-sys/src/lib.rs
@@ -17,9 +17,11 @@
 //!
 //! ## Presentation
 //!
-//! [Tomasz Stachowiak](https://github.com/h3r2tic) did a presentation at the Stockholm Rust Meetup on October 2019 about this project that goes through the tecnical details of how C++ to Rust bindings of `physx-sys` works:
+//! [Tomasz Stachowiak](https://github.com/h3r2tic) did a presentation at the Stockholm Rust Meetup on October 2019
+//! about this project that goes through the tecnical details of how C++ to Rust bindings of `physx-sys` works:
 //!
-//! [![](http://img.youtube.com/vi/RxtXGeDHu0w/0.jpg)](http://www.youtube.com/watch?v=RxtXGeDHu0w "An unholy fusion of Rust and C++ in physx-rs (Stockholm Rust Meetup, October 2019)")
+//! [![](http://img.youtube.com/vi/RxtXGeDHu0w/0.jpg)](http://www.youtube.com/watch?v=RxtXGeDHu0w "An unholy fusion of
+//! Rust and C++ in physx-rs (Stockholm Rust Meetup, October 2019)")
 //!
 //!
 //! ## Basic usage
@@ -76,20 +78,34 @@
 //!
 //! ## How it works
 //!
-//! The binding is generated using a custom C++ app written against clang's [libtooling](https://clang.llvm.org/docs/LibTooling.html). It queries the compiler's abstract syntax tree, and maps the C++ PhysX functions and types to Rust using heuristics chosen specifically for this SDK. It is not a general C++ <-> Rust binding generator, and using it on other projects *will* likely crash and burn.
+//! The binding is generated using a custom C++ app written against clang's
+//! [libtooling](https://clang.llvm.org/docs/LibTooling.html). It queries the compiler's abstract syntax tree, and maps
+//! the C++ PhysX functions and types to Rust using heuristics chosen specifically for this SDK. It is not a general
+//! C++ <-> Rust binding generator, and using it on other projects *will* likely crash and burn.
 //!
-//! Since C++ does not have a standardized and stable ABI, it's generally not safe to call it from Rust code; since PhysX exposes a C++ interface, we can't use it directly. That's why `physx-sys` generates both a Rust interface as well as a plain C wrapper. The C code is compiled into a static library at build time, and Rust then talks to C.
+//! Since C++ does not have a standardized and stable ABI, it's generally not safe to call it from Rust code; since
+//! PhysX exposes a C++ interface, we can't use it directly. That's why `physx-sys` generates both a Rust interface as
+//! well as a plain C wrapper. The C code is compiled into a static library at build time, and Rust then talks to C.
 //!
-//! In order to minimize the amount of work required to marshall data between the C wrapper and the original C++ API, we generate a **bespoke C wrapper for each build target**. The wrapper is based on metadata about structure layout extracted directly from compiling and running a tiny program against the PhysX SDK using the specific C++ compiler used in the build process.
+//! In order to minimize the amount of work required to marshall data between the C wrapper and the original C++ API, we
+//! generate a **bespoke C wrapper for each build target**. The wrapper is based on metadata about structure layout
+//! extracted directly from compiling and running a tiny program against the PhysX SDK using the specific C++ compiler
+//! used in the build process.
 //!
 //! The build process comprises a few steps:
 //!
-//! 1. The `pxbind` utility uses `clang` to extract metadata about PhysX functions and types, and generates partial Rust and C bindings as `physx_generated.hpp` and `physx_generated.rs`. Those contain all function definitions, and a small subset of types. It also generates a C++ utility called `structgen` by emitting `structgen.cpp`.
-//! 2. `structgen` is compiled against the PhysX SDK, and generates all the remaining type wrappers. For each struct, it queries the size and offset of its members, and generates `structgen_out.hpp` and `structgen_out.rs`. The types are "plain old data" structs which will perfectly match the memory layout of the C++ types.
+//! 1. The `pxbind` utility uses `clang` to extract metadata about PhysX functions and types, and generates partial
+//! Rust and C bindings as `physx_generated.hpp` and `physx_generated.rs`. Those contain all function definitions, and
+//! a small subset of types. It also generates a C++ utility called `structgen` by emitting `structgen.cpp`.
+//! 2. `structgen` is compiled against the PhysX SDK, and generates all the remaining type wrappers. For each struct, it
+//! queries the size and offset of its members, and generates `structgen_out.hpp` and `structgen_out.rs`. The types are
+//! "plain old data" structs which will perfectly match the memory layout of the C++ types.
 //! 3. All the generated C types are compiled together to form `physx_api`, a static library for Rust to link with.
 //! 4. The Rust wrapper is compiled, and linked with PhysX and the C wrapper.
 //!
-//! Steps *2..4* are performed completely automatically from within `build.rs`, while step *1* is only necessary when upgrading the PhysX SDK or modifying the generator. As such, building and running `pxbind` is a manual task, and is currently only supported on \*nix systems.
+//! Steps *2..4* are performed completely automatically from within `build.rs`, while step *1* is only necessary when
+//! upgrading the PhysX SDK or modifying the generator. As such, building and running `pxbind` is a manual task, and is
+//! currently only supported on \*nix systems.
 //!
 //! ## License
 //!
@@ -100,7 +116,9 @@
 //!
 //! at your option.
 //!
-//! Note that the [PhysX C++ SDK](https://github.com/NVIDIAGameWorks/PhysX) has it's [own BSD 3 license](https://gameworksdocs.nvidia.com/PhysX/4.1/documentation/physxguide/Manual/License.html) and depends on [additional C++ third party libraries](https://github.com/NVIDIAGameWorks/PhysX/tree/4.1/externals).
+//! Note that the [PhysX C++ SDK](https://github.com/NVIDIAGameWorks/PhysX) has it's
+//! [own BSD 3 license](https://gameworksdocs.nvidia.com/PhysX/4.1/documentation/physxguide/Manual/License.html) and
+//! depends on [additional C++ third party libraries](https://github.com/NVIDIAGameWorks/PhysX/tree/4.1/externals).
 //!
 //! ### Contribution
 //!
