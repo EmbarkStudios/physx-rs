77894e7fd711b0182231dd0d93b7e4635bc342f7
VZout <viktor.zoutman@embark-studios.com>
2020-04-29 13:35:56 +0200

    Re-added the sysroot flag

--- physx-sys/cc.rs
+++ physx-sys/cc.rs
@@ -236,7 +236,8 @@ fn add_common(ctx: &mut Context) {
     // These includes are used by pretty much everything so just add them first
     if ccenv.target_os == "android" {
         builder.define("ANDROID", None);
-        //builder.flag("--sysroot=/home/viktor/.NDK/arm64/sysroot/");
+        builder.flag("--sysroot=/home/viktor/.NDK/arm64/sysroot/");
+        builder.flag("--sysroot=/usr/local/android-ndk-r20/toolchains/llvm/prebuilt/linux-x86_64/sysroot/");
     }
 
     ctx.includes.push(shared_root.join("include"));
