58b7ec5b9f821587c054c265fc12aa092a8103aa
VZout <=>
2020-05-12 15:31:52 +0200

    linking

--- physx-sys/build.rs
+++ physx-sys/build.rs
@@ -21,10 +21,6 @@ include!("cc.rs");
 include!("cmake.rs");
 
 fn main() {
-    #[cfg(android)]
-    println!("cargo:rustc-link-lib=static-nobundle=stdc++");
-
-
     // Use the optimization level to determine the build profile to pass, we
     // don't use cfg!(debug_assertions) here because I'm not sure what happens
     // with that when build dependencies are configured to be debug and the
