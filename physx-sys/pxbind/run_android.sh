2c0347c88185d0b78d34b48d609f2b27a06a50fd
VZout <viktorzoutman@vzout.com>
2020-07-02 15:10:17 +0200

    CI and renamed ANDROID_HOME

--- physx-sys/pxbind/run_android.sh
+++ physx-sys/pxbind/run_android.sh
@@ -1,11 +1,11 @@
 cd ../src
 export PHYSX_ROOT_DIR=../PhysX/physx
-../pxbind/build/pxbind ../PhysX/physx/include/PxPhysicsAPI.h -- \
+../pxbind/build/pxbind $PHYSX_ROOT_DIR/include/PxPhysicsAPI.h -- \
     --target=aarch64-linux-android \
     -DNDEBUG=1 -x c++ \
     -DDISABLE_CUDA_PHYSX \
     -I../pxbind \
-    -I../PhysX/physx/include \
-    -I../PhysX/physx/../pxshared/include/ \
-    --sysroot=$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot \
+    -I$PHYSX_ROOT_DIR/include \
+    -I$PHYSX_ROOT_DIR/../pxshared/include/ \
+    --sysroot=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/sysroot \
     -fsigned-char
