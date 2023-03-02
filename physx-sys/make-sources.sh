175d5a59807676cfc4753b7e53ae528178df107e
Jake Shadle <jake.shadle@embark-studios.com>
2023-03-02 09:33:07 +0100

    Add usage to script

--- physx-sys/make-sources.sh
+++ physx-sys/make-sources.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# This script finds all of the cpp files in a <directory> and generates a rust
+# array of strings into physx-sys/sources/<name> that is used by the build script
+# to compile all of the source files when compiling physx-sys
+#
+# usage: make-sources.sh <physx c++ source directory path> <name>
 dir=$1
 file="$(dirname "$0")/sources/$2"
 
