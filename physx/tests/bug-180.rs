49a45978baca6bdf4793200b9d8e459067628570
Tom Solberg <me@sbg.dev>
2023-10-05 11:25:09 +0200

    remove unnecessary junk

--- physx/tests/bug-180.rs
+++ physx/tests/bug-180.rs
@@ -2,49 +2,6 @@ use physx::{base::RefCounted, prelude::*};
 
 type PxMaterial = physx::material::PxMaterial<()>;
 type PxShape = physx::shape::PxShape<(), PxMaterial>;
-type PxArticulationLink = physx::articulation_link::PxArticulationLink<(), PxShape>;
-type PxRigidStatic = physx::rigid_static::PxRigidStatic<(), PxShape>;
-type PxRigidDynamic = physx::rigid_dynamic::PxRigidDynamic<(), PxShape>;
-
-/// Next up, the simulation event callbacks need to be defined, and possibly an
-/// allocator callback as well.
-struct OnCollision;
-impl CollisionCallback for OnCollision {
-    fn on_collision(
-        &mut self,
-        _header: &physx_sys::PxContactPairHeader,
-        _pairs: &[physx_sys::PxContactPair],
-    ) {
-    }
-}
-struct OnTrigger;
-impl TriggerCallback for OnTrigger {
-    fn on_trigger(&mut self, _pairs: &[physx_sys::PxTriggerPair]) {}
-}
-
-struct OnConstraintBreak;
-impl ConstraintBreakCallback for OnConstraintBreak {
-    fn on_constraint_break(&mut self, _constraints: &[physx_sys::PxConstraintInfo]) {}
-}
-struct OnWakeSleep;
-impl WakeSleepCallback<PxArticulationLink, PxRigidStatic, PxRigidDynamic> for OnWakeSleep {
-    fn on_wake_sleep(
-        &mut self,
-        _actors: &[&physx::actor::ActorMap<PxArticulationLink, PxRigidStatic, PxRigidDynamic>],
-        _is_waking: bool,
-    ) {
-    }
-}
-
-struct OnAdvance;
-impl AdvanceCallback<PxArticulationLink, PxRigidDynamic> for OnAdvance {
-    fn on_advance(
-        &self,
-        _actors: &[&physx::rigid_body::RigidBodyMap<PxArticulationLink, PxRigidDynamic>],
-        _transforms: &[PxTransform],
-    ) {
-    }
-}
 
 #[test]
 fn test_double_free() {
